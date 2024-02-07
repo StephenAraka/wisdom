9f5442d683a2befd9ac2e9b9339579c9003062ed
Stephen Araka <stephen@Stephens-MacBook-Pro.local>
2024-02-07 23:00:03 +0300

    Modify images folder structure

--- src/constants/assets.js
+++ src/constants/assets.js
@@ -1,8 +1,11 @@
 export default {
-  calendarIconDarkTheme: require('../assets/icons/calendar-icon-white.png'),
-  menuIconDarkTheme: require('../assets/icons/menu-dots-white.png'),
-  shareIconDarkTheme: require('../assets/icons/share-icon-white.png'),
-  likeIconDarkTheme: require('../assets/icons/like-icon-white.png'),
-  lightIconDarkTheme: require('../assets/icons/light-icon-white.png'),
-  infoIconDarkTheme: require('../assets/icons/info-icon-white.png')
+  calendarIconDarkTheme: require('../assets/images/icons/calendar-icon-white.png'),
+  calendarIconLightTheme: require('../assets/images/icons/calendar-icon-black.png'),
+  menuIconDarkTheme: require('../assets/images/icons/menu-dots-white.png'),
+  menuIconLightTheme: require('../assets/images/icons/menu-dots-black.png'),
+  shareIconDarkTheme: require('../assets/images/icons/share-icon-white.png'),
+  likeIconDarkTheme: require('../assets/images/icons/like-icon-white.png'),
+  lightIconDarkTheme: require('../assets/images/icons/light-icon-white.png'),
+  infoIconDarkTheme: require('../assets/images/icons/info-icon-white.png'),
+  moonIcon: require('../assets/images/icons/moon-icon-white.png'),
 }
