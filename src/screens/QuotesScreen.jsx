8b0bc43a6b1e8c26155ae8d45c9c2bb42c01eb01
Stephen Araka <stephen@Stephens-MacBook-Pro.local>
2024-02-14 11:13:39 +0300

    Initialize state using system theme

--- src/screens/QuotesScreen.jsx
+++ src/screens/QuotesScreen.jsx
@@ -1,16 +1,23 @@
-import { StyleSheet, Text, View } from "react-native";
-import React, { useState } from "react";
+import { StyleSheet, View, useColorScheme } from "react-native";
+import React, { useState, useEffect } from "react";
 import jsonData from "../../data.json";
 import SwipeView from "../components/SwipeView";
 import Header from "../components/Header";
 import { connect } from "react-redux";
 import ScreenLayout from "../components/ScreenLayout";
 import Quote from "../components/Quote";
+import { initializeTheme } from "../context/actions/themeActions";
 
 const quotes = jsonData.quotes;
 
-const QuotesScreen = ({ isDarkTheme }) => {
+const QuotesScreen = ({ isDarkTheme, initializeTheme }) => {
   const [activeSubQuote, setActiveSubQuote] = useState(0);
+  const theme = useColorScheme();
+  const systemIsDarkTheme = theme === 'dark';
+
+  useEffect(() => {
+    initializeTheme(systemIsDarkTheme)
+  }, []);
 
   return (
     <ScreenLayout>
@@ -56,11 +63,15 @@ const QuotesScreen = ({ isDarkTheme }) => {
   );
 };
 
+const matchDispatchToProps = {
+  initializeTheme,
+};
+
 const mapStateToProps = (state) => ({
   isDarkTheme: state.theme.isDarkTheme,
 });
 
-export default connect(mapStateToProps)(QuotesScreen);
+export default connect(mapStateToProps, matchDispatchToProps)(QuotesScreen);
 
 const styles = StyleSheet.create({
   contentContainer: {
