a4a3dcecfc9b6036825ef421dc5f82064f4a69f9
Stephen Araka <stephen@Stephens-MacBook-Pro.local>
2024-03-21 23:58:56 +0300

    Render favorite quotes

--- src/screens/FavouriteQuotes.jsx
+++ src/screens/FavouriteQuotes.jsx
@@ -1,10 +1,35 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { useIsFocused } from '@react-navigation/native';
+import { getFavoriteQuotes } from "../utils/helpers";
 
 const FavouriteQuotes = () => {
+  const [favoriteQuotes, setFavoriteQuotes] = useState([]);
+
+  const fetchFavQuotes = async () => {
+    const favQuotes = await getFavoriteQuotes();
+    setFavoriteQuotes(favQuotes);
+  }
+
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    fetchFavQuotes();
+  }, [isFocused]);
+
+  const renderFavoriteItem = ({ item }) => {
+    const { message } = item;
+
+    return (
+      <View style={{ width: '100%' }}>
+        <Text style={{ color: 'black' }}>{message}</Text>
+      </View>
+    );
+  }
+  
   return (
-    <View>
-      <Text>Favourite quote</Text>
+    <View style={{ flex: 1, padding: 16 }}>
+      <FlatList data={favoriteQuotes} renderItem={renderFavoriteItem} />
     </View>
   );
 };
