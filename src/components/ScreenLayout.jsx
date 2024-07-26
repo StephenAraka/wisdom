62f941ef1aea0ba427ae5d0e388125b7649fcd41
agwadan <agwa.dan@gmail.com>
2024-07-26 09:56:05 +0300

    Add status bar

--- src/components/ScreenLayout.jsx
+++ src/components/ScreenLayout.jsx
@@ -1,18 +1,24 @@
-import { SafeAreaView } from 'react-native';
-import { connect } from 'react-redux';
-import React from 'react';
-import globalStyles from '../assets/styles/globalStyles';
+import { SafeAreaView, StatusBar } from "react-native";
+import { connect } from "react-redux";
+import React from "react";
+import globalStyles from "../assets/styles/globalStyles";
+import colors from "../constants/colors";
 
-const ScreenLayout = ({ isDarkTheme, children}) => {
+const ScreenLayout = ({ isDarkTheme, children }) => {
   return (
     <SafeAreaView style={globalStyles.SafeAreaContainer(isDarkTheme)}>
+      <StatusBar
+        barStyle={isDarkTheme ? "light-content" : "dark-content"}
+        backgroundColor={isDarkTheme ? "#000" : colors.white}
+        translucent={false}
+      />
       {children}
     </SafeAreaView>
-  )
-}
+  );
+};
 
 const mapStateToProps = (state) => ({
   isDarkTheme: state.theme.isDarkTheme,
 });
 
-export default connect(mapStateToProps) (ScreenLayout);
+export default connect(mapStateToProps)(ScreenLayout);
