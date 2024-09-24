0e5b0398ced65070d860d1c6921f63ab0cbc6f9a
agwadan <agwa.dan@gmail.com>
2024-09-24 13:17:27 +0300

    Add notification schedule

--- App.js
+++ App.js
@@ -1,41 +1,21 @@
-import React, {useEffect} from 'react';
-import { Button } from 'react-native';
-import store from './src/context/store';
-import { Provider } from 'react-redux';
-import Navigation from './src/navigation';
-import * as Notifications from 'expo-notifications';
+import React, { useEffect } from "react";
+
+import store from "./src/context/store";
+import { Provider } from "react-redux";
+import Navigation from "./src/navigation";
+
+import { scheduleDailyNotification } from "./src/utils/notifications";
 
 export default function App() {
-
   useEffect(() => {
-    async function getPermission() {
-      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-      if (status !== 'granted') {
-        alert('You need to enable notifications!');
-      }
-    }
-    getPermission();
+
+    scheduleDailyNotification();
   }, []);
 
-  const triggerNotification = async () => {
-    console.log('====================================');
-    console.log('clicked');
-    console.log('====================================');
-    await Notifications.scheduleNotificationAsync({
-      content: {
-        title: 'Test Notification',
-        body: 'This is a local notification!',
-      },
-      trigger: { seconds: 2}
-});
-  };
-
   return (
-    
+
     <Provider store={store}>
-      <Button title="Trigger Notification" onPress={triggerNotification} />
-      <Navigation/>
+      <Navigation />
     </Provider>
-    
   );
 }
--- src/utils/notifications.js
+++ src/utils/notifications.js
@@ -1,29 +1,15 @@
-import * as Notifications from 'expo-notifications';
-import React, { useEffect } from 'react';
-import { Button, View } from 'react-native';
+import * as Notifications from "expo-notifications";
 
-export default function App() {
-  useEffect(() => {
-    async function scheduleDailyNotification() {
-      await Notifications.scheduleNotificationAsync({
-        content: {
-          title: 'Daily Reminder',
-          body: 'This is your notification for today!',
-        },
-        trigger: {
-          hour: 8,
-          minute: 0,
-          repeats: true,
-},
-      });
-    }
-
-    scheduleDailyNotification();
-  }, []);
-
-  return (
-    <View>
-      <Button title="Trigger Daily Notification" onPress={() => {}} />
-    </View>
-  );
+export async function scheduleDailyNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "Today's dose of wisdom",
+      body: "Become wiser by taking a wisdom pill!",
+    },
+    trigger: {
+      hour: 8,
+      minute: 0,
+      repeats: true,
+    },
+  });
 }
