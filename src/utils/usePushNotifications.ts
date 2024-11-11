09750aa2dcf143764671c8a272e110d5f8bab513
Stephen Araka <stephen@Stephens-MacBook-Pro.local>
2024-11-11 23:05:56 +0300

    Add listeners and request permission

--- src/utils/usePushNotifications.ts
+++ src/utils/usePushNotifications.ts
@@ -3,15 +3,17 @@ import { useState, useEffect, useRef } from 'react';
 import * as Device from 'expo-device';
 import * as Notifications from 'expo-notifications';
 
-import { Constants } from 'expo-constants';
+import Constants from 'expo-constants';
 import { Platform } from 'react-native';
 
+// 1. Create interface
 export interface PushNotificationState {
   notification?: Notifications.Notification;
   expoPushToken?: Notifications.ExpoPushToken;
 };
 
 export const usePushNotifications = (): PushNotificationState => {
+  // 2. Define notifications handler
   Notifications.setNotificationHandler({
     handleNotification: async () => ({
       shouldPlaySound: false,
@@ -20,11 +22,54 @@ export const usePushNotifications = (): PushNotificationState => {
     }),
   });
 
+  // 3. Initialize notification state
   const [expoPushToken, setExpoPushToken] = useState<
     Notifications.ExpoPushToken | undefined
   >();
-
   const [notification, setNotification] = useState<
     Notifications.Notification | undefined
   >();
+
+  // 4. Add notification listeners
+  const notificationListener = useRef<Notifications.Subscription>();
+  const responseListener = useRef<Notifications.Subscription>();
+
+  // 5. Get permissions
+  async function registerForPushNotifications() {
+    let token;
+
+    if (Device.isDevice) {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+
+      let finalStatus = existingStatus;
+
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+
+      if (existingStatus !== "granted") {
+        alert('Failed to get push token');
+      }
+
+      token = await Notifications.getExpoPushTokenAsync({
+        projectId: Constants.expoConfig?.extra?.eas?.projectId,
+      });
+
+      if (Platform.OS === "android") {
+        Notifications.setNotificationChannelAsync("default", {
+          name: "default",
+          importance: Notifications.AndroidImportance.MAX,
+          vibrationPattern: [0, 250, 250, 250],
+          lightColor: "#FF231f7C",
+        })
+      }
+
+      return token;
+
+    } else {
+      console.log('Please use a physical device');
+      
+    }
+  }
 }
