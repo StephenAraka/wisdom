a96742dbeb7ec232d5eb034629aa3bac265c3cab
Stephen Araka <stephen@Stephens-MacBook-Pro.local>
2024-11-11 23:26:18 +0300

    Console logs to test

--- App.js
+++ App.js
@@ -2,8 +2,19 @@ import React from 'react';
 import store from './src/context/store';
 import { Provider } from 'react-redux';
 import Navigation from './src/navigation';
+import { usePushNotifications } from './src/utils/usePushNotifications';
 
 export default function App() {
+
+  const { expoPushToken, notification } = usePushNotifications();
+
+  const data = JSON.stringify(notification, undefined, 2);
+
+  // TODO: Remove logs
+  console.log('====================================');
+  console.log('data: => ', data);
+  console.log('expoPushToken: => ', expoPushToken);
+  console.log('====================================');
 
   return (
     
