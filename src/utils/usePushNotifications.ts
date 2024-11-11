ca1dace33f9fde04e53bfdefd8be40f63ba63975
Stephen Araka <stephen@Stephens-MacBook-Pro.local>
2024-11-11 22:50:11 +0300

    Create Notification state interface

---
+++ src/utils/usePushNotifications.ts
@@ -0,0 +1,12 @@
+import { useState, useEffect, useRef } from 'react';
+
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+
+import { Constants } from 'expo-constants';
+import { Platform } from 'react-native';
+
+export interface PushNotificationState {
+  notification?: Notifications.Notification;
+  expoPushToken?: Notifications.ExpoPushToken;
+};
