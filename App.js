cc0c080b82a5eeaed69f964e3ca4b229b34ecd28
StephenAraka <stevenaraka@gmail.com>
2025-03-03 21:04:24 +0300

    Setup notifications

--- App.js
+++ App.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import store from './src/context/store';
 import { Provider } from 'react-redux';
+import * as Notifications from 'expo-notifications';
 import Navigation from './src/navigation';
+import { requestNotificationPermission, scheduleDailyNotifications } from './src/utils/notifications';
 
 export default function App() {
+
+  useEffect(() => {
+    // Request notification permissions
+    requestNotificationPermission().then((hasPermission) => {
+      if (hasPermission) {
+        // Schedule daily notifications
+        scheduleDailyNotifications();
+      }
+    });
+
+    // Add a listener for incoming notifications
+    const subscription = Notifications.addNotificationReceivedListener((notification) => {
+      console.log('Notification received:', notification);
+    });
+
+    // Cleanup the listener on unmount
+    return () => subscription.remove();
+  }, []);
 
   return (
     
---
+++ src/utils/notifications.js
@@ -0,0 +1,47 @@
+import * as Notifications from 'expo-notifications';
+
+export async function requestNotificationPermission() {
+  const { status } = await Notifications.requestPermissionsAsync();
+  if (status !== 'granted') {
+    alert('You need to enable notifications for this feature to work.');
+  }
+}
+
+export async function scheduleDailyNotifications() {
+  // Configure the notification handler
+  Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+      shouldSetBadge: false,
+    }),
+  });
+
+  // Schedule 8 AM notification
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: 'Good Morning!',
+      body: 'It\'s 8 AM. Time to start your day with a dose of wisdom!',
+      sound: 'default',
+    },
+    trigger: {
+      hour: 8,
+      minute: 0,
+      repeats: true, // Repeat daily
+    },
+  });
+
+  // Schedule 8 PM notification
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: 'Good Evening!',
+      body: 'It\'s 9 PM. Time to wind down with a dose of wisdom!!',
+      sound: 'default',
+    },
+    trigger: {
+      hour: 21,
+      minute: 0,
+      repeats: true, // Repeat daily
+    },
+  });
+}
